78dbcbfc8cd6075ac5f65df19df863089b364350
‘e <389308748@qq.com>
2021-01-09 14:04:23 +0800

    21.安装脚手架

---
+++ app.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+    xxx
+    {{ msg }}
+  </div>
+</template>
+
+<script>
+export default {
+    data(){
+        return {
+            msg:"123456"
+        }
+    }
+}
+</script>
+
+<style>
+    div{
+        background-color: red;
+    }
+</style>
